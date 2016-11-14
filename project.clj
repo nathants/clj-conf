38231a4939b19c934e107a132218c28d747b30f3
nathants <me@nathants.com>
2016-11-13 23:09:34 -0800

    migrate out of clj-util

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject clj-confs "0.0.1-SNAPSHOT"
+  :description "edn configuration loader and consumer"
+  :url "http://github.com/nathants/clj-conf"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
